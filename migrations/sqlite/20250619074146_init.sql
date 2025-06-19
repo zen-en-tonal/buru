2a7c92ba9aed90162d7da291d35b044a2e40f642
Takeru KODAMA <zen.en.tonal@gmail.com>
2025-06-19 08:37:32 +0000

    add: migration scripts

---
+++ migrations/sqlite/20250619074146_init.sql
@@ -0,0 +1,45 @@
+-- Add migration script here
+
+CREATE TABLE images (
+    hash TEXT PRIMARY KEY,
+    source TEXT
+);
+
+CREATE TABLE image_metadatas (
+    image_hash TEXT,
+    width INTEGER NOT NULL,
+    height INTEGER NOT NULL,
+    format TEXT NOT NULL,
+    color_type TEXT NOT NULL,
+    file_size INTEGER NOT NULL,
+    created_at TEXT NOT NULL,
+    duration REAL,
+    PRIMARY KEY (image_hash),
+    FOREIGN KEY (image_hash) REFERENCES images(hash) ON DELETE CASCADE
+);
+
+CREATE INDEX idx_image_metadatas_created_at_desc
+ON image_metadatas (created_at DESC);
+
+CREATE TABLE tags (
+    name TEXT PRIMARY KEY
+);
+
+CREATE TABLE image_tags (
+    image_hash TEXT,
+    tag_name TEXT,
+    PRIMARY KEY (image_hash, tag_name),
+    FOREIGN KEY (image_hash) REFERENCES images(hash) ON DELETE CASCADE,
+    FOREIGN KEY (tag_name) REFERENCES tags(name) ON DELETE CASCADE
+);
+
+CREATE VIEW image_with_metadata AS
+SELECT *
+FROM images
+LEFT JOIN image_metadatas ON images.hash = image_metadatas.image_hash;
+
+CREATE TABLE tag_counts (
+    tag_name TEXT PRIMARY KEY,
+    count INTEGER NOT NULL,
+    FOREIGN KEY (tag_name) REFERENCES tags(name) ON DELETE CASCADE
+);
